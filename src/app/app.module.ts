83d4dd725378bbcd822a9a2f2d7d5c365efe796f
SilverJ1369 <35865016+SilverJ1369@users.noreply.github.com>
2023-10-03 19:59:14 -0500

    further progress on sidebar and task list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule
